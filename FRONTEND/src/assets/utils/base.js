6e7d2dc0e8409f8a82ce7ec517d52285281fedcc
Leandrodeleon22 <leandrodeleon22@gmail.com>
2023-10-07 19:43:08 -0400

    edited localhost to 6000

--- FRONTEND/src/assets/utils/base.js
+++ FRONTEND/src/assets/utils/base.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-  baseURL: "http://localhost:3000/api/v1/tasks",
+  baseURL: "http://localhost:6000/api/v1/tasks",
 });
 
 export default customFetch;
