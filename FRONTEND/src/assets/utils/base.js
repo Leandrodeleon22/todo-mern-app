c391fbca293da1ff929aed7ec99659d8b45b7952
Leandrodeleon22 <leandrodeleon22@gmail.com>
2023-10-09 19:08:14 -0400

    final Frontend deploy

--- FRONTEND/src/assets/utils/base.js
+++ FRONTEND/src/assets/utils/base.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const customFetch = axios.create({
   // baseURL: "http://localhost:6000/api/v1/tasks",
-  baseURL: "https://temp-todo-aoo.onrender.com//api/v1/tasks",
+  baseURL: "https://temp-todo-aoo.onrender.com/api/v1/tasks",
 });
 
 export default customFetch;
